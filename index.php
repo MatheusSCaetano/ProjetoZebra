cdc5910364e8ba14f2da951c7153a1fe23ecadea
Matheus Caetano <caetanomatheus142@gmail.com>
2025-05-22 11:09:08 -0400

    Criado escopo e marcacoes

---
+++ index.php
@@ -0,0 +1,107 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keyword" content="palavras,chave,do,meu,site">
+    <meta name="description" content="Descricao do site">
+
+    <link href="estilo/style.css" rel="stylesheet">
+
+    <title>Projeto</title>
+</head>
+<body>
+    <header>
+        <div class="center"> 
+        <div class='logo'><img src="img\logo.png" width="100"> </div>
+        <nav class="desktop">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Serviços</a></li>
+            <li><a href="">Contatos</a></li>
+        </ul>
+        </nav> 
+        </div><!-- center -->
+    </header>
+
+    <section class="banner-principal"> 
+    <div class="center">    
+    <form>
+    <input type='text' name='email' placeholder="Digite seu email" required>
+    <input type='password' name='senha'  placeholder="Digite sua senha" required>
+    <input type="submit" name="acao" value="Cadastrar"  >
+    </form>
+    </div><!-- center -->
+    </section>
+
+    <section class="area-principal"> 
+    <h1>Baixe a planilha de controle financeiro aqui</h1>
+    </section>
+
+    
+    <section class="produtos"> 
+    <h2>Estratégias</h2>
+    <div class="center">
+
+    <div class="box-produto">
+    <img src="img\produto1.png " width="60">
+    <h2> Orçamentos mensais</h2>
+    <p>Crie seu planejamento financeiro, definindo limites de gastos e alertas, para garantir que 
+        você terá disciplina para conquistar seu plano! </p>    
+</div><!-- box produto-->        
+    
+    <div class="box-produto">
+    <img src="img\produto2.png " width="60">  
+    <h2>Objetivos</h2>
+    <p>Seja qual for o seu sonho, o Minhas Economias ajuda a planejá-lo. Defina e acompanhe seus
+         objetivos no app.</p>
+
+    </div><!-- box produto--> 
+
+    <div class="box-produto">
+    <img src="img\produto3.png " width="60">    
+    <h2>Dashboard</h2>
+<p>Tenha acesso às análises dos seus gastos por período e pelas categorias mais relevantes, 
+    recebendo insights para melhorar sua vida financeira!</p>
+    </div> <!-- box produto-->
+
+
+    </div><!-- center -->
+
+    </section><!-- section produtos -->
+        
+    <section class="artigos">
+
+    <div class="center"> 
+
+    <div class="box-artigo">
+        <h2>Artigo</h2> 
+    </div><!-- box artigo-->
+   
+    <div class="box-artigo">
+        <h2>Artigo</h2> 
+    </div><!-- box artigo-->
+
+    <div class="box-artigo">
+        <h2>Artigo</h2> 
+    </div><!-- box artigo-->
+
+    </div><!-- center -->
+
+    </section><!-- section artigos-->
+
+    <footer>
+    <div class="center">
+
+    <p> Todos os direitos reservados</p>
+
+    </div><!-- center -->
+
+    </footer>
+
+
+
+</body>
+</html>

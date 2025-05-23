1ef2b8372dfff632d5cd3a2cbe6f47de228e4176
Matheus Caetano <caetanomatheus142@gmail.com>
2025-05-22 22:50:40 -0400

    Estilizacao pagina principal

--- index.php
+++ index.php
@@ -7,15 +7,25 @@
     <meta name="keyword" content="palavras,chave,do,meu,site">
     <meta name="description" content="Descricao do site">
 
-    <link href="estilo/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" />
 
     <title>Projeto</title>
 </head>
 <body>
     <header>
         <div class="center"> 
-        <div class='logo'><img src="img\logo.png" width="100"> </div>
-        <nav class="desktop">
+        <div class="logo left"><img src="img\logo.png" width="70"> </div>
+        <nav class="desktop  ">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Serviços</a></li>
+            <li><a href="">Contatos</a></li>
+            <input type="button" value="entrar"/>
+            <input type="button" value="cadastrar"/>
+        </ul>
+        </nav> 
+        <nav class="mobile right">
         <ul>
             <li><a href="">Home</a></li>
             <li><a href="">Sobre</a></li>
@@ -28,42 +38,51 @@
 
     <section class="banner-principal"> 
     <div class="center">    
-    <form>
+    <!-- <form>
     <input type='text' name='email' placeholder="Digite seu email" required>
     <input type='password' name='senha'  placeholder="Digite sua senha" required>
     <input type="submit" name="acao" value="Cadastrar"  >
-    </form>
+    </form> -->
     </div><!-- center -->
     </section>
 
     <section class="area-principal"> 
+    <div class="center"> 
     <h1>Baixe a planilha de controle financeiro aqui</h1>
-    </section>
+    </div> <!-- center-->    
+    </section> <!-- area principal-->
 
     
     <section class="produtos"> 
-    <h2>Estratégias</h2>
     <div class="center">
 
+    <h2>Estratégias</h2>
+
     <div class="box-produto">
-    <img src="img\produto1.png " width="60">
-    <h2> Orçamentos mensais</h2>
-    <p>Crie seu planejamento financeiro, definindo limites de gastos e alertas, para garantir que 
-        você terá disciplina para conquistar seu plano! </p>    
-</div><!-- box produto-->        
+        <div class="topo">
+        <img src="img\produto1.png " width="40">
+        <h2> Orçamentos mensais</h2>
+    </div><!-- class topo-->
+        <p>Crie seu planejamento financeiro, definindo limites de gastos e alertas, para garantir que 
+        você terá disciplina para conquistar seu plano! </p>        
+    </div><!-- box produto-->        
     
     <div class="box-produto">
-    <img src="img\produto2.png " width="60">  
+        <div class="topo">
+    <img src="img\produto2.png " width="40">  
     <h2>Objetivos</h2>
+    </div><!-- box produto-->
     <p>Seja qual for o seu sonho, o Minhas Economias ajuda a planejá-lo. Defina e acompanhe seus
          objetivos no app.</p>
 
     </div><!-- box produto--> 
 
     <div class="box-produto">
-    <img src="img\produto3.png " width="60">    
+    <div class="topo">
+    <img src="img\produto3.png " width="40">    
     <h2>Dashboard</h2>
-<p>Tenha acesso às análises dos seus gastos por período e pelas categorias mais relevantes, 
+    </div><!-- box produto-->
+    <p>Tenha acesso às análises dos seus gastos por período e pelas categorias mais relevantes, 
     recebendo insights para melhorar sua vida financeira!</p>
     </div> <!-- box produto-->
 
@@ -72,19 +91,20 @@
 
     </section><!-- section produtos -->
         
+
     <section class="artigos">
 
     <div class="center"> 
 
-    <div class="box-artigo">
+    <div class="w33 left box-artigo">
         <h2>Artigo</h2> 
     </div><!-- box artigo-->
    
-    <div class="box-artigo">
+    <div class="w33 left box-artigo">
         <h2>Artigo</h2> 
     </div><!-- box artigo-->
 
-    <div class="box-artigo">
+    <div class="w33 left box-artigo">
         <h2>Artigo</h2> 
     </div><!-- box artigo-->
 
